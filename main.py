fb091c4aebe27362eef3028487e73a698b5d5fe7
Cory Latschkowski <koree@redhat.com>
2022-10-11 13:50:24 -0500

    fix: model name

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import os
 import pandas as pd
 import pickle
 
+model_name = os.environ.get('MODEL_NAME', 'model')
 
 class Main:
     def __init__(self):
-        self.model_name = "Main"
+        self.model_name = model_name
 
         # load the model from disk
-        model_name = os.environ.get('MODEL_NAME', 'model')
-        self.model = pickle.load(open(model_name, "rb"))
+        self.model = pickle.load(open(self.model_name, "rb"))
 
     def predict(self, X, features_names):
         df = pd.DataFrame(data=X, columns=features_names)
